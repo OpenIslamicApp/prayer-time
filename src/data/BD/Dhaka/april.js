7f3ca9c559c95321a54bd3bc0c7446355179451b
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-04-03 20:00:34 +0600

    🔖📕added

---
+++ src/data/BD/Dhaka/april.js
@@ -0,0 +1,67 @@
+export const april = [
+  {
+    sehri: "00:00 AM",
+    fazar: "00:00 AM",
+    iftar: "00:00 PM",
+  }, // don't edit or change this object
+  {
+    sehri: "04:27 AM",
+    fazar: "04:33 AM",
+    iftar: "18:19 PM",
+  },
+  {
+    sehri: "04:27 AM",
+    fazar: "04:33 AM",
+    iftar: "18:19 PM",
+  },
+  {
+    sehri: "04:27 AM",
+    fazar: "04:33 AM",
+    iftar: "18:19 PM",
+  },
+  {
+    sehri: "04:26 AM",
+    fazar: "04:32 AM",
+    iftar: "18:19 PM",
+  },
+  {
+    sehri: "04:24 AM",
+    fazar: "04:30 AM",
+    iftar: "18:20 PM",
+  },
+  {
+    sehri: "04:24 AM",
+    fazar: "04:21 AM",
+    iftar: "18:20 PM",
+  },
+  {
+    sehri: "04:23 AM",
+    fazar: "04:29 AM",
+    iftar: "18:21 PM",
+  },
+  {
+    sehri: "04:22 AM",
+    fazar: "04:28 AM",
+    iftar: "18:22 PM",
+  },
+  {
+    sehri: "04:21 AM",
+    fazar: "04:27 AM",
+    iftar: "18:21 PM",
+  },
+  {
+    sehri: "04:20 AM",
+    fazar: "04:26 AM",
+    iftar: "18:22 PM",
+  },
+  {
+    sehri: "04:19 AM",
+    fazar: "04:25 AM",
+    iftar: "18:22 PM",
+  },
+  {
+    sehri: "04:18 AM",
+    fazar: "04:25 AM",
+    iftar: "18:23 PM",
+  },
+];
