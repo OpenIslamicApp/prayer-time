058c3950e2e2b6b591350528df26546f7778cfad
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-04-03 19:59:43 +0600

    🛠update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TimeBox from "./components/TimeBox";
+import { timeKeyBangla } from "./data/timeKey/bangla";
+import { timeKeyEnglish } from "./data/timeKey/english";
+import Header from "./layout/Header";
+import Menu from "./layout/Menu";
+import { HomeContainer } from "./styles/style";
 
 function App() {
+  const [menu, setMenu] = useState(false);
+  const [language, setLanguage] = useState("bangla");
+  const LANGU =
+    language === "bangla"
+      ? timeKeyBangla
+      : language === "english"
+      ? timeKeyEnglish
+      : timeKeyEnglish;
+  const [Time, setTime] = useState();
+  setInterval(() => {
+    setTime(new Date().toLocaleTimeString());
+  }, 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HomeContainer>
+      <Header Time={Time} menu={menu} setMenu={setMenu} />
+      <Menu menu={menu} language={language} setLanguage={setLanguage} />
+      <div className="body">
+        <TimeBox name={LANGU[0]} time="06:00 AM" />
+        <TimeBox name={LANGU[1]} time="06:00 AM" />
+        <TimeBox name={LANGU[2]} time="06:00 AM" />
+        <TimeBox name={LANGU[3]} time="06:00 AM" />
+        <TimeBox name={LANGU[4]} time="06:00 AM" />
+        <TimeBox name={LANGU[5]} time="06:00 AM" />
+        <TimeBox name={LANGU[6]} time="06:00 AM" />
+      </div>
+    </HomeContainer>
   );
 }
 
