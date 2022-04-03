1797443916e78b83bc761d4aaec6c78ae04a551e
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-04-03 20:00:11 +0600

    🔖✨added and done

---
+++ src/components/TimeBox.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function TimeBox({ name, time }) {
+  return (
+    <Container>
+      <div className="prayer_name">{name}</div>
+      <div className="prayer_Time">{time}</div>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: var(--bg-sec);
+  box-shadow: var(--bg-sec-shadow);
+  border-radius: 20px;
+  /* aspect-ratio: 1/1; */
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .prayer {
+    &_name {
+      font-size: calc(13px + 2vmin);
+      font-weight: 600;
+      color: var(--color-gray);
+    }
+    &_Time {
+      font-size: calc(17.5px + 2vmin);
+      font-weight: 600;
+      color: var(--color-gray-deep);
+    }
+  }
+`;
