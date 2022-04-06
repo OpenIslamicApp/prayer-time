5cb2dfbefff816d153f754c565e309391471f30b
Hasanul Banna Himel <84923500+Hasanul-Banna-Himel@users.noreply.github.com>
2022-04-06 15:36:09 +0600

    4 months added

---
+++ october.js
@@ -0,0 +1,291 @@
+export const october = [
+    {
+      sehri: "00:00 AM",
+      fajar: "00:00 AM",
+      sunrise: "00:00 AM",
+      dhuhur: "00:00 PM",
+      asr: "00:00 PM",
+      maghrib: "00:00 PM",
+      isha: "00:00 PM",
+    }, // don't edit or change this object
+    {
+      sehri: "04:29 AM",
+      fajar: "04:39 AM",
+      sunrise: "05:49 AM",
+      dhuhur: "11:51 AM",
+      asr: "04:06 PM",
+      magrib: "05:49 PM",
+      isha: "07:02 PM",
+    }, // 1
+    {
+      sehri: "04:29 AM",
+      fajar: "04:39 AM",
+      sunrise: "05:49 AM",
+      dhuhur: "11:51 AM",
+      asr: "04:06 PM",
+      magrib: "05:49 PM",
+      isha: "07:02 PM",
+    }, // 2
+    {
+      sehri: "04:29 AM",
+      fajar: "04:39 AM",
+      sunrise: "05:49 AM",
+      dhuhur: "11:51 AM",
+      asr: "04:06 PM",
+      magrib: "05:49 PM",
+      isha: "07:02 PM",
+    }, // 3
+    {
+      sehri: "04:29 AM",
+      fajar: "04:39 AM",
+      sunrise: "05:49 AM",
+      dhuhur: "11:51 AM",
+      asr: "04:06 PM",
+      magrib: "05:49 PM",
+      isha: "07:02 PM",
+    }, // 4
+    {
+      sehri: "04:30 AM",
+      fajar: "04:40 AM",
+      sunrise: "05:51 AM",
+      dhuhur: "11:50 AM",
+      asr: "04:03 PM",
+      magrib: "05:45 PM",
+      isha: "06:58 PM",
+    }, // 5
+    {
+      sehri: "04:30 AM",
+      fajar: "04:40 AM",
+      sunrise: "05:51 AM",
+      dhuhur: "11:50 AM",
+      asr: "04:03 PM",
+      magrib: "05:45 PM",
+      isha: "06:58 PM",
+    }, // 6
+    {
+      sehri: "04:30 AM",
+      fajar: "04:40 AM",
+      sunrise: "05:51 AM",
+      dhuhur: "11:50 AM",
+      asr: "04:03 PM",
+      magrib: "05:45 PM",
+      isha: "06:58 PM",
+    }, // 7
+    {
+      sehri: "04:30 AM",
+      fajar: "04:40 AM",
+      sunrise: "05:51 AM",
+      dhuhur: "11:50 AM",
+      asr: "04:03 PM",
+      magrib: "05:45 PM",
+      isha: "06:58 PM",
+    }, // 8
+    {
+      sehri: "04:30 AM",
+      fajar: "04:40 AM",
+      sunrise: "05:51 AM",
+      dhuhur: "11:50 AM",
+      asr: "04:03 PM",
+      magrib: "05:45 PM",
+      isha: "06:58 PM",
+    }, // 9
+    {
+      sehri: "04:32 AM",
+      fajar: "04:42 AM",
+      sunrise: "05:53 AM",
+      dhuhur: "11:49 AM",
+      asr: "03:59 PM",
+      magrib: "05:41 PM",
+      isha: "06:54 PM",
+    }, // 10
+    {
+      sehri: "04:32 AM",
+      fajar: "04:42 AM",
+      sunrise: "05:53 AM",
+      dhuhur: "11:49 AM",
+      asr: "03:59 PM",
+      magrib: "05:41 PM",
+      isha: "06:54 PM",
+    }, // 11
+    {
+      sehri: "04:32 AM",
+      fajar: "04:42 AM",
+      sunrise: "05:53 AM",
+      dhuhur: "11:49 AM",
+      asr: "03:59 PM",
+      magrib: "05:41 PM",
+      isha: "06:54 PM",
+    }, // 12
+    {
+      sehri: "04:32 AM",
+      fajar: "04:42 AM",
+      sunrise: "05:53 AM",
+      dhuhur: "11:49 AM",
+      asr: "03:59 PM",
+      magrib: "05:41 PM",
+      isha: "06:54 PM",
+    }, // 13
+    {
+      sehri: "04:32 AM",
+      fajar: "04:42 AM",
+      sunrise: "05:53 AM",
+      dhuhur: "11:49 AM",
+      asr: "03:59 PM",
+      magrib: "05:41 PM",
+      isha: "06:54 PM",
+    }, // 14
+    {
+      sehri: "04:34 AM",
+      fajar: "04:44 AM",
+      sunrise: "05:56 AM",
+      dhuhur: "11:48 AM",
+      asr: "03:55 PM",
+      magrib: "05:36 PM",
+      isha: "06:50 PM",
+    }, // 15
+    {
+      sehri: "04:34 AM",
+      fajar: "04:44 AM",
+      sunrise: "05:56 AM",
+      dhuhur: "11:48 AM",
+      asr: "03:55 PM",
+      magrib: "05:36 PM",
+      isha: "06:50 PM",
+    }, // 16
+    {
+      sehri: "04:34 AM",
+      fajar: "04:44 AM",
+      sunrise: "05:56 AM",
+      dhuhur: "11:48 AM",
+      asr: "03:55 PM",
+      magrib: "05:36 PM",
+      isha: "06:50 PM",
+    }, // 17
+    {
+      sehri: "04:34 AM",
+      fajar: "04:44 AM",
+      sunrise: "05:56 AM",
+      dhuhur: "11:48 AM",
+      asr: "03:55 PM",
+      magrib: "05:36 PM",
+      isha: "06:50 PM",
+    }, // 18
+    {
+      sehri: "04:34 AM",
+      fajar: "04:44 AM",
+      sunrise: "05:56 AM",
+      dhuhur: "11:48 AM",
+      asr: "03:55 PM",
+      magrib: "05:36 PM",
+      isha: "06:50 PM",
+    }, // 19
+    {
+      sehri: "04:36 AM",
+      fajar: "04:46 AM",
+      sunrise: "05:58 AM",
+      dhuhur: "11:47 AM",
+      asr: "03:51 PM",
+      magrib: "05:32 PM",
+      isha: "06:46 PM",
+    }, // 20
+    {
+      sehri: "04:36 AM",
+      fajar: "04:46 AM",
+      sunrise: "05:58 AM",
+      dhuhur: "11:47 AM",
+      asr: "03:51 PM",
+      magrib: "05:32 PM",
+      isha: "06:46 PM",
+    }, // 21
+    {
+      sehri: "04:36 AM",
+      fajar: "04:46 AM",
+      sunrise: "05:58 AM",
+      dhuhur: "11:47 AM",
+      asr: "03:51 PM",
+      magrib: "05:32 PM",
+      isha: "06:46 PM",
+    }, // 22
+    {
+      sehri: "04:36 AM",
+      fajar: "04:46 AM",
+      sunrise: "05:58 AM",
+      dhuhur: "11:47 AM",
+      asr: "03:51 PM",
+      magrib: "05:32 PM",
+      isha: "06:46 PM",
+    }, // 23
+    {
+      sehri: "04:36 AM",
+      fajar: "04:46 AM",
+      sunrise: "05:58 AM",
+      dhuhur: "11:47 AM",
+      asr: "03:51 PM",
+      magrib: "05:32 PM",
+      isha: "06:46 PM",
+    }, // 24
+    {
+      sehri: "04:38 AM",
+      fajar: "04:48 AM",
+      sunrise: "06:00 AM",
+      dhuhur: "11:46 AM",
+      asr: "03:48 PM",
+      magrib: "05:28 PM",
+      isha: "06:42 PM",
+    }, // 25
+    {
+      sehri: "04:38 AM",
+      fajar: "04:48 AM",
+      sunrise: "06:00 AM",
+      dhuhur: "11:46 AM",
+      asr: "03:48 PM",
+      magrib: "05:28 PM",
+      isha: "06:42 PM",
+    }, // 26
+    {
+      sehri: "04:38 AM",
+      fajar: "04:48 AM",
+      sunrise: "06:00 AM",
+      dhuhur: "11:46 AM",
+      asr: "03:48 PM",
+      magrib: "05:28 PM",
+      isha: "06:42 PM",
+    }, // 27
+    {
+      sehri: "04:38 AM",
+      fajar: "04:48 AM",
+      sunrise: "06:00 AM",
+      dhuhur: "11:46 AM",
+      asr: "03:48 PM",
+      magrib: "05:28 PM",
+      isha: "06:42 PM",
+    }, // 28
+    {
+      sehri: "04:38 AM",
+      fajar: "04:48 AM",
+      sunrise: "06:00 AM",
+      dhuhur: "11:46 AM",
+      asr: "03:48 PM",
+      magrib: "05:28 PM",
+      isha: "06:42 PM",
+    }, // 29
+    {
+      sehri: "04:38 AM",
+      fajar: "04:48 AM",
+      sunrise: "06:00 AM",
+      dhuhur: "11:46 AM",
+      asr: "03:48 PM",
+      magrib: "05:28 PM",
+      isha: "06:42 PM",
+    }, // 30
+    {
+      sehri: "04:38 AM",
+      fajar: "04:48 AM",
+      sunrise: "06:00 AM",
+      dhuhur: "11:46 AM",
+      asr: "03:48 PM",
+      magrib: "05:28 PM",
+      isha: "06:42 PM",
+    }, // 31
+  ];
+  
