434a2e31d3fbe74facba4a488337c1d01a9ebef5
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2022-12-09 20:58:09 +0600

    🛠️date added

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 export default function Header({ menu, setMenu }) {
   const [Time, setTime] = useState();
@@ -7,11 +8,79 @@ export default function Header({ menu, setMenu }) {
   }, 1000);
 
   return (
-    <header>
+    <Container>
       {Time}
+      <p>
+        Today: {new Date().getDate()}/{new Date().getMonth()}/
+        {new Date().getFullYear()}
+      </p>
       <nav onClick={() => setMenu(!menu)}>
         <div className={`nav_icon ${menu ? "open" : ""}`}></div>
       </nav>
-    </header>
+    </Container>
   );
 }
+
+const Container = styled.header`
+  background-color: var(--bg-sec);
+  box-shadow: var(--bg-sec-shadow);
+  border-radius: 20px;
+  padding: 20px;
+  font-size: calc(20px + 1vmin);
+  font-weight: 900;
+  color: var(--color-th);
+  text-align: center;
+  position: sticky;
+  top: 0;
+  z-index: 100;
+  position: relative;
+  & > p {
+    font-size: calc(9.5px + 1vmin);
+  }
+  nav {
+    position: absolute;
+    top: 0;
+    right: 0;
+    background-color: var(--bg-sec);
+    box-shadow: var(--bg-sec-shadow);
+    border-radius: 50%;
+    height: 100%;
+    aspect-ratio: 1/1;
+    display: grid;
+    place-items: center;
+    .nav_icon {
+      background-color: var(--color-gray);
+      height: 2px;
+      width: 25px;
+      position: relative;
+      transition: all 0.2s ease-in-out;
+      &::before,
+      &::after {
+        content: "";
+        position: absolute;
+        background-color: var(--color-gray);
+        height: 2px;
+        width: 25px;
+        left: 0;
+        opacity: 1;
+      }
+      &::before {
+        top: -10px;
+      }
+      &::after {
+        bottom: -10px;
+      }
+      &.open {
+        height: 0;
+        &::before {
+          top: -0px;
+          transform: rotate(45deg);
+        }
+        &::after {
+          top: -0px;
+          transform: rotate(-45deg);
+        }
+      }
+    }
+  }
+`;
--- src/styles/style.js
+++ src/styles/style.js
@@ -6,66 +6,6 @@ export const HomeContainer = styled.main`
   margin: 0 auto;
   min-height: 100vh;
   padding: 20px;
-  header {
-    background-color: var(--bg-sec);
-    box-shadow: var(--bg-sec-shadow);
-    border-radius: 20px;
-    padding: 20px;
-    font-size: calc(20px + 1vmin);
-    font-weight: 900;
-    color: var(--color-th);
-    text-align: center;
-    position: sticky;
-    top: 0;
-    z-index: 100;
-    position: relative;
-    nav {
-      position: absolute;
-      top: 0;
-      right: 0;
-      background-color: var(--bg-sec);
-      box-shadow: var(--bg-sec-shadow);
-      border-radius: 50%;
-      height: 100%;
-      aspect-ratio: 1/1;
-      display: grid;
-      place-items: center;
-      .nav_icon {
-        background-color: var(--color-gray);
-        height: 2px;
-        width: 25px;
-        position: relative;
-        transition: all 0.2s ease-in-out;
-        &::before,
-        &::after {
-          content: "";
-          position: absolute;
-          background-color: var(--color-gray);
-          height: 2px;
-          width: 25px;
-          left: 0;
-          opacity: 1;
-        }
-        &::before {
-          top: -10px;
-        }
-        &::after {
-          bottom: -10px;
-        }
-        &.open {
-          height: 0;
-          &::before {
-            top: -0px;
-            transform: rotate(45deg);
-          }
-          &::after {
-            top: -0px;
-            transform: rotate(-45deg);
-          }
-        }
-      }
-    }
-  }
   .body {
     padding-top: 40px;
     gap: 20px;
